d5fa743566c42966a00176ae1d49a5a1161df939
Manuel Björn Gomes Jóhannsson <FS403719@fss.is>
2016-10-11 09:29:17 +0000

    undefined error

--- components/App.js
+++ components/App.js
@@ -1,10 +1,65 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var App = React.createClass({
-  render: function(){
-    return <span>Hot module reloading!!</span>
-  }
+	render: function() {
+		return (
+			<div className="commentBox">
+			<h1>Comments</h1>
+			<CommentList data={this.props.data}/>
+			<CommentForm />
+			</div>
+		);
+	}
 });
 
-export default App;
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+var CommentForm = React.createClass({
+	render: function() {
+		return(
+			<div className="commentForm">
+			Hello, world! I am a CommentForm.
+			</div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+	rawMarkup: function() {
+		var md = new Remarkable();
+		var rawMarkup = md.render(this.props.children.toString());
+		return { __html: rawMarkup};
+	},
+	render: function() {
+		return(
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+			</div>
+		);
+	}
+});
+
+export default CommentBox;
