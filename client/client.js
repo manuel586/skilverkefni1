72cc97fa936013847f5fc0a1debea156725dc393
Manuel Björn Gomes Jóhannsson <FS403719@fss.is>
2016-10-17 10:08:01 +0000

    ha

--- client/client.js
+++ client/client.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from '../components/App'
 
-render(<App url="/api/comments" />, document.getElementById("app"));
+render(<App url="/api/comments" pollInterval={2000}/>, document.getElementById("app"));
--- components/App.js
+++ components/App.js
@@ -7,11 +7,31 @@ var data = [
 ];
 
 var App = React.createClass({
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url:this.props.url,
+			dataType: "json",
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function() {
 		return (
 			<div className="commentBox">
 			<h1>Comments</h1>
-			<CommentList data={this.props.data}/>
+			<CommentList data={this.state.data}/>
 			<CommentForm />
 			</div>
 		);
@@ -37,9 +57,11 @@ var CommentList = React.createClass({
 var CommentForm = React.createClass({
 	render: function() {
 		return(
-			<div className="commentForm">
-			Hello, world! I am a CommentForm.
-			</div>
+			<form className="commentForm">
+				<input type="text" placeholder="Your name" />
+				<input type="text" placeholder="Say something..." />
+				<input type="Submit" value="Post" />
+			</form>
 		);
 	}
 });
